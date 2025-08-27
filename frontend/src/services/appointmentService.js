258dc7cfb9b9e5bcf64b494a0d8971749dc8d3a9
Bhushan_Raut <rautbhushan517@gmail.com>
2025-08-27 12:22:30 +0530

    jhgj

--- frontend/src/services/appointmentService.js
+++ frontend/src/services/appointmentService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'https://helth-42uq.onrender.com/api';
+const API_URL = process.env.REACT_APP_API_URL || 'https://helth-1.onrender.com/api';
 
 // Create axios instance
 const api = axios.create({
